7dae2821e766eef9b67d0b6f5c885720fd5bc8a6
Devil7DK <dineshthangavel47@gmail.com>
2023-04-29 15:43:04 +0530

    Implement camera controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.scss';
 
+import { CameraControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import React from 'react';
 
-import { Box } from './Box';
+import { Box } from './components';
 
 export const App: React.FC = () => {
     return (
@@ -11,6 +12,8 @@ export const App: React.FC = () => {
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
             <Box position={[0, 0, 0]} height={10} length={10} width={10} />
+
+            <CameraControls distance={20} azimuthAngle={10} polarAngle={1} />
         </Canvas>
     );
 };
